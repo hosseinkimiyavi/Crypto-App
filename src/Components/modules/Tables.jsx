55e140935025f2f55b684da86baaf756da355ff1
Hossein Kimiyavi <hosseinkimiyavi@gmail.com>
2024-07-08 18:56:07 +0330

    make table for informaition of coines

--- src/Components/modules/Tables.jsx
+++ src/Components/modules/Tables.jsx
@@ -1,10 +1,46 @@
 import React from 'react'
-
+import chartUp from "../../assets/chart-up.svg"
+import chartDown from "../../assets/chart-down.svg"
 function Tables({coines}) {
     console.log(coines)
   return (
-    <div>Tables</div>
+    <div>
+      <table className='text-white m-auto'>
+        <thead>
+          <tr>
+            <th>Coin</th>
+            <th>Name</th>
+            <th>Price</th>
+            <th>24h</th>
+            <th>Total Volume</th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+          {coines.map((coines)=>(
+            <TableRow coines={coines} key={coines.id} />
+          ))}
+            
+        </tbody>
+      </table>
+    </div>
   )
 }
 
 export default Tables
+
+const TableRow = ({coines:{image ,symbol ,name,current_price,total_volume ,price_change_percentage_24h:price_change}})=>{
+  return(<tr>
+    <td>
+      <div>
+    <img src={image} alt={symbol} className='h-8' />
+    <span>{symbol.toUpperCase()}</span>
+    </div>
+    </td>
+    <td>{name}</td>
+    <td>{current_price.toLocaleString()}</td>
+    <td>{price_change.toFixed(2)}%</td>
+    <td>{total_volume.toLocaleString()}</td>
+    <td><img src={price_change>0?chartUp:chartDown} alt="" /></td>
+    </tr>)
+}
